da0d46e6da3449e35cebee2bf3f52452df211200
Aymerick <aymerick@fotonauts.com>
2013-11-18 01:18:01 +0100

    refactoring

---
+++ node.go
@@ -0,0 +1,11 @@
+package main
+
+type Node struct {
+    Id   byte
+    Name string
+}
+
+type INode interface {
+    handleData(data []byte)
+    dump()
+}
---
+++ serial_reader.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "github.com/tarm/goserial"
+    "log"
+    "io"
+)
+
+const (
+    PORT_NAME = "/dev/tty.usbserial-A1014IM4"
+    PORT_BAUD = 57600
+    LF_CHAR   = 10
+)
+
+type SerialReader struct {
+    io.ReadWriteCloser
+}
+
+func NewSerialReader() *SerialReader {
+    // @todo Settingfy port
+    conf := &serial.Config{Name: PORT_NAME, Baud: PORT_BAUD}
+    ser, err := serial.OpenPort(conf)
+    if err != nil {
+        log.Panic(err)
+    }
+
+    return &SerialReader{ser}
+}
+
+func (ser *SerialReader) readLine() string {
+    result   := make([]byte, 0)
+    lastRead := make([]byte, 1)
+
+    // read byte by byte until the Line Feed character
+    for lastRead[0] != LF_CHAR {
+        n, err := ser.Read(lastRead)
+        if (err != nil) || (n != 1) {
+            log.Panic(err)
+        }
+
+        result = append(result, lastRead[0])
+    }
+
+    return string(result)
+}
